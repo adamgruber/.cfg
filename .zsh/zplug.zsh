623ca9ebc045caba451b2bd2666c370ead8d5bb8
Adam Gruber <talknmime@gmail.com>
2021-11-03 13:25:47 -0400

    Update zplug.zsh
    Fix fast syntax highlighting repo

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -1,8 +1,8 @@
 source $HOME/.zplug/init.zsh
 
 # Feature rich syntax highlighting
-# https://github.com/zdharma/fast-syntax-highlighting
-zplug "zdharma/fast-syntax-highlighting", defer:2
+# https://github.com/zdharma-continuum/fast-syntax-highlighting
+zplug "zdharma-continuum/fast-syntax-highlighting", defer:2
 
 # macOS utility commands
 # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/osx
